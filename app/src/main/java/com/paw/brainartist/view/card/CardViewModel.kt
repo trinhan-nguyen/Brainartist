f5f72decb547292c46c3cd2ad0040b509b64f546
Tri Nhan Nguyen <ngtrnhan1205@gmail.com>
2020-12-15 22:21:26 -0500

    Set Up Quiz Settings Screen: add quiz settings screen

--- app/src/main/java/com/paw/brainartist/view/card/CardViewModel.kt
+++ app/src/main/java/com/paw/brainartist/view/card/CardViewModel.kt
@@ -3,22 +3,34 @@ package com.paw.brainartist.view.card
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.paw.brainartist.models.CardDeck
-import com.paw.brainartist.models.PlayingCard
+import com.paw.brainartist.model.CardDeck
+import com.paw.brainartist.model.PlayingCard
 
 class CardViewModel: ViewModel() {
     private val cardDeck = CardDeck()
     private val _card = MutableLiveData<PlayingCard>()
     val card: LiveData<PlayingCard>
         get() = _card
+    private val _eventFinishMemorizing = MutableLiveData<Boolean>()
+    val eventFinishMemorizing: LiveData<Boolean>
+        get() = _eventFinishMemorizing
 
     init {
+        _eventFinishMemorizing.value = false
         cardDeck.shuffle()
         onNext()
     }
 
     fun onNext() {
         _card.value = cardDeck.getCurrentCard()
-        cardDeck.nextCard()
+        if (cardDeck.isLastCard()) {
+            _eventFinishMemorizing.value = true
+        } else {
+            cardDeck.nextCard()
+        }
+    }
+
+    fun onFinishMemorizingComplete() {
+        _eventFinishMemorizing.value = false
     }
 }
